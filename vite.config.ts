b1eacb48c55d22a91f74159015dc3539160aaf72
Visar <visarmullaj07@gmail.com>
2026-06-20 16:03:54 +0200

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
   plugins: [react(), tailwindcss()],
   build: {
     chunkSizeWarningLimit: 550,
