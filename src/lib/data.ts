c64fb919efc185a910bc744f8ba9542cd8744ae5
Visar <visarmullaj07@gmail.com>
2026-06-21 18:56:52 +0200

    load student commit

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,8 +1,18 @@
-import { type OverviewData } from "@shared/types/types";
+import {
+  StudentStats,
+  StudentStatsFile,
+  type OverviewData,
+} from "@shared/types/types";
 
 export async function loadOverview(): Promise<OverviewData[]> {
   const response = await fetch(`/data/overview.json`);
   return readJson<OverviewData[]>(response);
+}
+
+export async function loadStudent(studentId: string): Promise<StudentStats> {
+  const response = await fetch(`/data/students${studentId.slice(0, 7)}.json`);
+  const file = await readJson<StudentStatsFile>(response);
+  return file[studentId];
 }
 
 async function readJson<T>(response: Response): Promise<T> {
